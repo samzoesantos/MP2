30214f1ba6831bd52e0b474031b5f13b97252bca
CJsalvino <vergilsalvino@gmail.com>
2024-01-19 11:28:32 +0800

    API is live, pushing to see if it works for the whole team

--- JS files/show_doctors-API.js
+++ JS files/show_doctors-API.js
@@ -1,20 +1,67 @@
-async function getData(data) {
-  let my_obj = await fetch(data);
-  let my_text = await my_obj.text();
-  let doctors = JSON.parse(my_text);
+$(document).ready(function () {
+  $("#seeDoctors").click(function () {
+    async function getData(data) {
+      $("#doctor_list").empty();
+      let my_obj = await fetch(data);
+      let my_text = await my_obj.text();
+      let doctors = JSON.parse(my_text);
 
-  for (let i = 0; i < doctors.length; i++) {
-    let row =
-      "<tr><th>Name</th><th>Specialization</th><th>Availability</th></tr><tr><td>" +
-      doctors[i].name +
-      "</td><td>" +
-      doctors[i].specialization +
-      "</td><td>" +
-      doctors[i].availability +
-      "</td></tr>";
+      let head =
+        "<tr><th>Name</th><th>Specialization</th><th>Availability</th></tr>";
+      $("#doctor_list").append(head);
 
-    $("#doctor_list").append(row);
-  }
-}
+      for (let i = 0; i < doctors.length; i++) {
+        let row =
+          "<tr><td>" +
+          doctors[i].name +
+          "</td><td>" +
+          doctors[i].specialization +
+          "</td><td>" +
+          doctors[i].availability +
+          "</td></tr>";
 
-getData("http://localhost:3005/api/doctors");
+        $("#doctor_list").append(row);
+      }
+    }
+
+    getData("https://api-render-xvn7.onrender.com/api/doctors");
+  });
+});
+
+$(document).ready(function () {
+  $("#spec").change(function () {
+    async function getData(data) {
+      $("#doctor_list").empty();
+      let my_obj = await fetch(data);
+      let my_text = await my_obj.text();
+      let doctors = JSON.parse(my_text);
+
+      let selected = $("#spec").val();
+      let filtered_docs = [];
+      let head =
+        "<tr><th>Name</th><th>Specialization</th><th>Availability</th></tr>";
+      $("#doctor_list").append(head);
+
+      for (let i = 0; i < doctors.length; i++) {
+        if (doctors[i].specialization == selected) {
+          filtered_docs.push(doctors[i]);
+
+          let row =
+            "<tr><td>" +
+            doctors[i].name +
+            "</td><td>" +
+            doctors[i].specialization +
+            "</td><td>" +
+            doctors[i].availability +
+            "</td></tr>";
+
+          $("#doctor_list").append(row);
+        }
+      }
+    }
+
+    getData(
+      "https://api-render-xvn7.onrender.com/api/doctors/" + $("#spec").val()
+    );
+  });
+});
